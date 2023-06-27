e8cfcd810028d32d2490283c1af66be0985c1c8b
Jobayer98 <jobayerrahman0220@gmail.com>
2023-06-28 04:15:46 +0600

    add banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
+import React from "react";
+import {
+  HomeOutlined,
+  ContactsOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import Banner from "./components/Banner/Banner";
+
+const { Content, Footer, Sider } = Layout;
+
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  HomeOutlined,
+  ContactsOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-export default App
+  return (
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            style={{
+              textAlign: "center",
+              background: colorBgContainer,
+            }}
+          >
+            <Banner />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,40 @@
+import { Typography } from "antd";
+const { Title, Paragraph } = Typography;
+import { Typewriter } from "react-simple-typewriter";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="flex items-center gap-8 mx-2">
+      <div className="flex  flex-col text-left w-1/2">
+        <h2 className="text-2xl font-bold mb-2">Hello, I am</h2>
+        <h1 className="text-7xl font-bold mb-5">Sergio Gadot</h1>
+        <Title level={4} style={{ fontWeight: "bold", marginBottom: "20px" }}>
+          I am Passionate{" "}
+          <Typewriter
+            words={["Programmer", "Developer"]}
+            cursor
+            cursorStyle="|"
+            loop={false}
+            typeSpeed={70}
+            deleteSpeed={70}
+            delaySpeed={1000}
+          />
+        </Title>
+        <Paragraph className="text-lg">
+          I design and develop services for customers of all sizes, specializing
+          in creating stylish, modern websites, web services and online stores.
+        </Paragraph>
+        <button className="btn_resume">Download Resume</button>
+      </div>
+      <div className="w-1/2">
+        <img
+          src="https://sergio-react.vercel.app/static/img/home-banner.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
